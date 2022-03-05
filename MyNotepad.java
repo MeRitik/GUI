1f83722bec57d3ba0e19b5e27c549a7607a1f04c
Ritik Ranjan <60888272+MeRitik@users.noreply.github.com>
2022-03-05 21:52:20 +0530

    Update MyNotepad.java

--- MyNotepad.java
+++ MyNotepad.java
@@ -1,34 +1,47 @@
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.Font;
-import java.awt.PrintJob;
+
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JTextArea;
+import javax.swing.UIManager;
 import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 import javax.swing.JMenu;
 import javax.swing.JRadioButtonMenuItem;
+import javax.swing.JScrollPane;
+
 import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.print.PrinterException;
 import java.awt.print.PrinterJob;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import javax.swing.JColorChooser;
 import javax.swing.JFileChooser;
+import javax.swing.JCheckBoxMenuItem;
 
 
 public class MyEditor extends JFrame {
-	int fsize = 10;
-	StringBuilder fontName = new StringBuilder();;
+	int fsize = 12;
+	boolean isSavedFile, stateChanged = false;
+	
+	File currentFile = null;
 	
 	private JPanel contentPane;
 
@@ -56,9 +69,11 @@ public class MyEditor extends JFrame {
 		setTitle("Untitled - My Editor");
 		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\ritik\\Desktop\\notepad.jpg"));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
 		
+		// set opening position on screen
+		setBounds(100, 50, 500, 500);
 		
+		isSavedFile = false;
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 		
@@ -105,6 +120,12 @@ public class MyEditor extends JFrame {
 		JMenuItem mntmNewMenuItem_8 = new JMenuItem("Paste");
 		mnNewMenu_1.add(mntmNewMenuItem_8);
 		
+		JMenuItem mntmNewMenuItem_12 = new JMenuItem("Select all");
+		mnNewMenu_1.add(mntmNewMenuItem_12);
+		
+		JMenuItem mntmNewMenuItem_9 = new JMenuItem("Date/Time");
+		mnNewMenu_1.add(mntmNewMenuItem_9);
+		
 		JMenu mnNewMenu_2 = new JMenu("View");
 		menuBar.add(mnNewMenu_2);
 		
@@ -118,6 +139,11 @@ public class MyEditor extends JFrame {
 		ButtonGroup sz = new ButtonGroup();
 		sz.add(rdbtnmntmNewRadioItem_1);
 		sz.add(rdbtnmntmNewRadioItem);
+		
+		JCheckBoxMenuItem wrapped = new JCheckBoxMenuItem("Word wrap");
+		wrapped.setSelected(true);
+		mnNewMenu_2.add(wrapped);
+		
 		
 		rdbtnmntmNewRadioItem.addActionListener((e)->{
 			setSize(500, 500);
@@ -154,9 +180,18 @@ public class MyEditor extends JFrame {
 		JRadioButtonMenuItem rdbtnmntmNewRadioItem_4 = new JRadioButtonMenuItem("Monotype Corsiva");
 		mnNewMenu_4.add(rdbtnmntmNewRadioItem_4);
 		
+		JRadioButtonMenuItem rdbtnmntmNewRadioItem_13 = new JRadioButtonMenuItem("Microsoft Himalaya");
+		mnNewMenu_4.add(rdbtnmntmNewRadioItem_13);
+		
+		JRadioButtonMenuItem rdbtnmntmNewRadioItem_14 = new JRadioButtonMenuItem("Calibri");
+		mnNewMenu_4.add(rdbtnmntmNewRadioItem_14);
+		
 		fontName.add(rdbtnmntmNewRadioItem_2);
 		fontName.add(rdbtnmntmNewRadioItem_3);
 		fontName.add(rdbtnmntmNewRadioItem_4);
+		fontName.add(rdbtnmntmNewRadioItem_13);
+		fontName.add(rdbtnmntmNewRadioItem_14);
+		
 		
 		JMenu mnNewMenu_5 = new JMenu("Font size");
 		mnNewMenu_3.add(mnNewMenu_5);
@@ -164,11 +199,11 @@ public class MyEditor extends JFrame {
 		ButtonGroup fontSize = new ButtonGroup();
 		
 		JRadioButtonMenuItem rdbtnmntmNewRadioItem_5 = new JRadioButtonMenuItem("10");
-		rdbtnmntmNewRadioItem_5.setSelected(true);
 		mnNewMenu_5.add(rdbtnmntmNewRadioItem_5);
 		fontSize.add(rdbtnmntmNewRadioItem_5);
 		
 		JRadioButtonMenuItem rdbtnmntmNewRadioItem_6 = new JRadioButtonMenuItem("12");
+		rdbtnmntmNewRadioItem_6.setSelected(true);
 		mnNewMenu_5.add(rdbtnmntmNewRadioItem_6);
 		fontSize.add(rdbtnmntmNewRadioItem_6);
 		
@@ -203,25 +238,44 @@ public class MyEditor extends JFrame {
 		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.X_AXIS));
 		
 		JTextArea textArea = new JTextArea();
-		contentPane.add(textArea);
+//		contentPane.add(textArea);
 		textArea.setLineWrap(true);
-		textArea.setFont(new Font("Arial", Font.PLAIN, 10));
+		textArea.setFont(new Font("Arial", Font.PLAIN, fsize));
+		
+		// Scroll 
+		JScrollPane scroll = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		contentPane.add(scroll);
+		
+
 		
 		mntmNewMenuItem_1.addActionListener((ae)->{
+			// Checking if a file is opened
 			JFileChooser jfc = new JFileChooser();
-			int x = jfc.showSaveDialog(this);
-			FileWriter file = null;
-			
-			if(x == JFileChooser.APPROVE_OPTION) {
+			if(isSavedFile) {
 				try {
-					String path = jfc.getSelectedFile().getAbsolutePath();
-					String name = jfc.getSelectedFile().getName();
-					setTitle(name + " - My Notepad");
-					file = new FileWriter(path);
+					FileWriter file = new FileWriter(currentFile.getAbsoluteFile());
 					file.write(textArea.getText());
 					file.close();
-				}catch(IOException ie) {}
+				}catch(IOException io) {}
+			}else {
+				int x = jfc.showSaveDialog(this);
+				FileWriter file = null;
 				
+				if(x == JFileChooser.APPROVE_OPTION) {
+					try {
+						String path = jfc.getSelectedFile().getAbsolutePath();
+						String name = jfc.getSelectedFile().getName();
+						setTitle(name + " - My Notepad");
+						file = new FileWriter(path);
+						
+						currentFile = new File(path);
+						
+						file.write(textArea.getText());
+						file.close();
+						isSavedFile = true;
+					}catch(IOException ie) {}
+					
+				}
 			}
 		});
 		
@@ -234,8 +288,6 @@ public class MyEditor extends JFrame {
 			if(x == JFileChooser.APPROVE_OPTION) {
 				try {
 					String path = jfc.getSelectedFile().getAbsolutePath();
-					String name = jfc.getSelectedFile().getName();
-					setTitle(name + " - My Notepad");
 					file = new FileWriter(path);
 					file.write(textArea.getText());
 					file.close();
@@ -254,15 +306,115 @@ public class MyEditor extends JFrame {
 					BufferedReader bReader = new BufferedReader(reader);
 					setTitle(jfc.getSelectedFile().getName());
 					
+					// Setting the current file
+					currentFile = new File(jfc.getSelectedFile().getAbsolutePath());
+					
 					textArea.read(bReader, null);
 					bReader.close();
+					isSavedFile = true;
 				}catch(IOException ie) {}
 			}
 		});
 		
-		mntmNewMenuItem_5.addActionListener((ae)->{
-			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		textArea.addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {
+				stateChanged = true;
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				stateChanged = true;
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				stateChanged = true;
+			}
 		});
+		
+		mntmNewMenuItem_5.addActionListener((ae)->{
+			if(textArea.getText().isEmpty()) {
+				System.exit(0);
+			}
+			
+			if(stateChanged) {
+				JFrame jf = new JFrame("MyEditor");
+				jf.setVisible(true);
+				jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				jf.setBounds(600, 300, 503, 126);
+				jf.setTitle("MyEditor");
+				JPanel contentPane = new JPanel();
+				contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+				jf.setContentPane(contentPane);
+				contentPane.setLayout(null);
+						
+				JLabel text = new JLabel("Do you want to save changes to " + getTitle());
+				text.setFont(new Font("Microsoft Himalaya", Font.PLAIN, 24));
+				text.setBounds(106, 19, 373, 24);
+				contentPane.add(text);
+						
+				JButton btnNewButton = new JButton("Don't Save");
+				btnNewButton.setBounds(180, 53, 99, 26);
+				contentPane.add(btnNewButton);
+				
+				JButton btnNewButton_1 = new JButton("Save");
+				btnNewButton_1.setBounds(52, 53, 99, 26);
+				contentPane.add(btnNewButton_1);
+						
+				JButton btnNewButton_2 = new JButton("Cancel");
+				btnNewButton_2.setBounds(306, 53, 99, 26);
+				contentPane.add(btnNewButton_2);
+						
+						
+				JLabel lblNewLabel = new JLabel(UIManager.getIcon("OptionPane.warningIcon"));
+				lblNewLabel.setBounds(52, 12, 55, 37);
+				contentPane.add(lblNewLabel);
+						
+				btnNewButton.addActionListener((e)->{
+					System.exit(0);
+				});
+						
+				btnNewButton_1.addActionListener((e)->{
+					// code for saving the file
+					// Checking if a file is opened
+					JFileChooser jfc = new JFileChooser();
+					if(isSavedFile) {
+						try {
+							FileWriter file = new FileWriter(currentFile.getAbsoluteFile());
+							file.write(textArea.getText());
+							file.close();
+						}catch(IOException io) {}
+					}else {
+						int x = jfc.showSaveDialog(this);
+						FileWriter file = null;
+								
+						if(x == JFileChooser.APPROVE_OPTION) {
+							try {
+								String path = jfc.getSelectedFile().getAbsolutePath();
+								String name = jfc.getSelectedFile().getName();
+								setTitle(name + " - My Notepad");
+								file = new FileWriter(path);
+										
+								currentFile = new File(path);
+										
+								file.write(textArea.getText());
+								file.close();
+								isSavedFile = true;
+								System.exit(0);
+							}catch(IOException ie) {}
+									
+						}
+					}
+				});
+						
+				btnNewButton_2.addActionListener((e)->{
+					jf.dispose();
+				});
+			}
+		});
+		
 		
 		mntmNewMenuItem_11.addActionListener((e)->{
 			var color = JColorChooser.showDialog(this, "Choose font color", getForeground());
@@ -279,6 +431,14 @@ public class MyEditor extends JFrame {
 		
 		rdbtnmntmNewRadioItem_4.addActionListener((e)->{
 			textArea.setFont(new Font("Monotype Corsiva", Font.PLAIN, fsize));
+		});
+		
+		rdbtnmntmNewRadioItem_13.addActionListener((e)->{
+			textArea.setFont(new Font("Microsoft Himalya", Font.PLAIN, fsize));
+		});
+		
+		rdbtnmntmNewRadioItem_14.addActionListener((e)->{
+			textArea.setFont(new Font("Calibri", Font.PLAIN, fsize));
 		});
 		
 		rdbtnmntmNewRadioItem_5.addActionListener((e)->{
@@ -325,10 +485,14 @@ public class MyEditor extends JFrame {
 		
 		
 		mntmNewMenuItem_4.addActionListener((e)->{
-			// Not completed
-			
 			var pj = PrinterJob.getPrinterJob();
 			boolean x = pj.printDialog();
+			
+			if(x == true) {
+				try {
+					pj.print();
+				}catch(PrinterException pe) {}
+			}
 		});
 		
 		mntmNewMenuItem_6.addActionListener((e)->{
@@ -347,5 +511,36 @@ public class MyEditor extends JFrame {
 			var color = JColorChooser.showDialog(textArea, "Choose background color", textArea.getBackground());
 			textArea.setBackground(color);
 		});
+		
+		
+		// WindowStateListener to check if the frame is full screen or not
+		// if it is full screen then select full screen mode else normal mode
+		addWindowStateListener((we)->{
+			int isMaximized = getExtendedState();
+			
+			if(isMaximized == MAXIMIZED_BOTH) {
+				rdbtnmntmNewRadioItem_1.setSelected(true);
+			}else {
+				rdbtnmntmNewRadioItem.setSelected(true);
+			}
+		});
+		
+		wrapped.addActionListener((e)->{
+			boolean isWrapped = textArea.getLineWrap();
+			
+			textArea.setLineWrap(!isWrapped);
+		});
+		
+		mntmNewMenuItem_9.addActionListener((e)->{
+			DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+			LocalDateTime now = LocalDateTime.now();  
+			textArea.setText(textArea.getText() + dtf.format(now));
+		});
+		
+		mntmNewMenuItem_12.addActionListener((e)->{
+			textArea.selectAll();
+		});
+		
 	}
+
 }
